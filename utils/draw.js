37831acc37887c06a60b83755df6f9c36f556568
YaliixxG <529008421@qq.com>
2022-05-19 17:39:13 +0800

    update: demo10

--- utils/draw.js
+++ utils/draw.js
@@ -19,3 +19,25 @@ const polygon = (startX, startY, size, n, ctx) => {
     }
     ctx.closePath();
 };
+
+/**
+ * @description 绘制五角星
+ * @param {Number} startX 中心点横坐标
+ * @param {Number} startY 中心点纵坐标
+ * @param {Number} size 中心点到远顶点的距离
+ * @param {Object} ctx 上下文对象
+ */
+const star = (startX, startY, size, ctx) => {
+    ctx.beginPath();
+    for (let i = 0; i < 5; i++) {
+        ctx.lineTo(
+            Math.cos(((i * 72 + 18) * Math.PI) / 180) * size + startX,
+            -Math.sin(((i * 72 + 18) * Math.PI) / 180) * size + startY
+        );
+        ctx.lineTo(
+            Math.cos(((i * 72 + 54) * Math.PI) / 180) * (size / 2) + startX,
+            -Math.sin(((i * 72 + 54) * Math.PI) / 180) * (size / 2) + startY
+        );
+    }
+    ctx.closePath();
+};
